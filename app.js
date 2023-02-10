6b63176d1eabfacb448c62e19aabe3e847c537ca
shnaboi <shane.karner@gmail.com>
2023-02-09 23:39:30 -0500

    Add initial form and basic style

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+let myLibrary = [];
+
 function Book(title, author, pages, yn) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.yn = yn;
     this.info = () => `${title} by ${author}, ${pages} pages, ${yn}`;
+}
+
+function addBook() {
+    
+}
+function pushBook() {
+    myLibrary.push()
 }
 
 // function Fiction() {};
