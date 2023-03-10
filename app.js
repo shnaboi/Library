0e1f69dddd82cedc414ccd0b29bc850fad60ccb1
shnaboi <shane.karner@gmail.com>
2023-03-09 23:50:05 -0500

    Add basic style for cards

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-let myLibrary = [{"title": "bible", "author": "Jesus", "pages": "69420", "yn": "no"}];
+const BookDiv = document.getElementsByClassName('book-div')
+
+let myLibrary = [{
+    title: "bible", 
+    author: "Jesus", 
+    pages: "69420", 
+    yn: "no"
+    }];
 
 function Book(title, author, pages, yn) {
     this.title = title;
@@ -15,12 +22,14 @@ function pushBook() {
     myLibrary.push()
 }
 
+
+
 // function Fiction() {};
 // Fiction.prototype = Object.create(Book.prototype);
 
 let test = new Book('a', 'b', 69, 'yes');
 
-
+BookDiv.textContent = "test";
 
 console.log(test);
 console.log(myLibrary)
